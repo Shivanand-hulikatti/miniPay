b683a7292696a9a99a6fd7bb14ab1e96bf312934
Shivanand-hulikatti <01fe23bcs268@kletech.ac.in>
2024-07-28 17:56:18 +0530

    added post to listen

--- backend/index.js
+++ backend/index.js
@@ -3,10 +3,10 @@ const cors = require('cors');
 const jwt = require('jsonwebtoken');
 const zod = require('zod');
 const rootRouter = require('./routes/index');
-
+const port =process.env.port ||3000;
 const app = express();
 app.use(express.json());
 app.use(cors());
 
 app.use('/api/v1',rootRouter);
-app.listen(3000);
+app.listen(port);
