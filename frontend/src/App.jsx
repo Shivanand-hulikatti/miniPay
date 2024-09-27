96af8f81c2632d78e3283bd501b9b63d0eeadd0f
Shivanand-hulikatti <01fe23bcs268@kletech.ac.in>
2024-09-27 16:08:03 +0530

    made / page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -9,7 +9,7 @@ export default function App() {
     <>
       <BrowserRouter>
         <Routes>
-          <Route path="/signup" element={<Signup />} />
+          <Route path="/" element={<Signup />} />
           <Route path="/signin" element={<Signin />} />
           <Route path="/dashboard" element={<Dashboard />} />
           <Route path="/send" element={<SendMoney />} />
